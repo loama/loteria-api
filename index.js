e491b4a6d9623059d9bdce92e0056a19714c18d0
Jaime Lopez <loama18@gmail.com>
2018-10-05 19:25:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+const cors = require('cors')({
+  origin: true,
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  cors(request, response, () => {
+    response.status(200).send({test: 'Hello'});
+  })
+});
